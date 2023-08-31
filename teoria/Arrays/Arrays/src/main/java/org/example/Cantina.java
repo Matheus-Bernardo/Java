5b1ef24acda7fc9453ede07d7de80dbec2dfa1a8
Matheus Henrique Lourenço Bernardo <matheus.bernardo@inatel.br>
2023-08-31 20:59:08 -0300

    exercicioArray

---
+++ teoria/Arrays/Arrays/src/main/java/org/example/Cantina.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Cantina {
+
+    String nome;
+    Salgado[] salgado = new Salgado[3];
+
+    void addSalgado (Salgado novoSalgado){
+
+        for (int i = 0; i < salgado.length; i++) {
+            if (salgado[i] == null){
+                salgado[i] = novoSalgado;
+                break;
+            }
+        }
+    }
+
+    void mostrarInfo(){
+        for (int i = 0; i < salgado.length ; i++) {
+            System.out.println("Salgado "+(i+1)+ ": "+ salgado[i].nome);
+        }
+    }
+}
