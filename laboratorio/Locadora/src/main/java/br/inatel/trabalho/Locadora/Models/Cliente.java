fe1ccccbe9b415436c010fc6eb433291f0840d80
Matheus Henrique Lourenço Bernardo <matheus.bernardo@inatel.br>
2023-11-03 14:55:16 -0300

    remoção parcial get e setters

--- laboratorio/Locadora/src/main/java/br/inatel/trabalho/Locadora/Models/Cliente.java
+++ laboratorio/Locadora/src/main/java/br/inatel/trabalho/Locadora/Models/Cliente.java
@@ -2,53 +2,42 @@ package br.inatel.trabalho.Locadora.Models;
 
 public class Cliente {
     //atributos cliente
-    private String cpf;
+    private String cpf;//chave primaria
     private String nome;
     private String endereco;
     private String telefone;
     private String email;
 
+    //construtor
 
-    //getter e setters
 
+    public Cliente(String cpf, String nome, String endereco, String telefone, String email) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.email = email;
+    }
 
+    //getters
     public String getCpf() {
         return cpf;
     }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
     public String getNome() {
         return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
     }
 
     public String getEndereco() {
         return endereco;
     }
 
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
     public String getTelefone() {
         return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+
 }
