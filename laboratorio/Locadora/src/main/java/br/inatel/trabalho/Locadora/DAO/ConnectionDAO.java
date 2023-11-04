f9110c4cc6fcfd10c4e9aecb9276ebdee52a60ad
Matheus Henrique Lourenço Bernardo <matheus.bernardo@inatel.br>
2023-11-03 23:26:01 -0300

    comentarios

--- laboratorio/Locadora/src/main/java/br/inatel/trabalho/Locadora/DAO/ConnectionDAO.java
+++ laboratorio/Locadora/src/main/java/br/inatel/trabalho/Locadora/DAO/ConnectionDAO.java
@@ -9,25 +9,23 @@ public class ConnectionDAO {
     Statement st; //declaração(query) código em sql
     ResultSet rs;//resposta do banco
 
+    //parametros usados para o banco local
     String dataBase = "Locadora";//nome do banco de dados que iremos conectar
     String user = "root";//nome do user
     String senha = "1234";
-
     String url = "jdbc:mysql://localhost:3306/" + dataBase + "?useTimezone=true&serverTimezone=UTC&useSSL=false&allowPublicKeyRetrieval=true";
 
 
     public void conectaNoBanco(){
         try {
             conexao = DriverManager.getConnection(url,user,senha);
-            System.out.println("deu certo");
+            System.out.println("Conexão feita");
 
         }catch (SQLException exc){
             System.out.println("Erro:" + exc.getMessage());
 
         }
     }
-
-
 
 }
 
