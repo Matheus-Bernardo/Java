145d832afca7e5b13d074f521687161d0361a3f7
Matheus Henrique Lourenço Bernardo <matheus.bernardo@inatel.br>
2023-11-03 23:15:39 -0300

    organização

--- laboratorio/Locadora/src/main/java/br/inatel/trabalho/Locadora/Models/Ator.java
+++ laboratorio/Locadora/src/main/java/br/inatel/trabalho/Locadora/Models/Ator.java
@@ -5,19 +5,15 @@ public class Ator {
     private String nomeAtor;
 
     //getter e setters
-
     public int getIdAtor() {
         return idAtor;
     }
-
     public void setIdAtor(int idAtor) {
         this.idAtor = idAtor;
     }
-
     public String getNomeAtor() {
         return nomeAtor;
     }
-
     public void setNomeAtor(String nomeAtor) {
         this.nomeAtor = nomeAtor;
     }
