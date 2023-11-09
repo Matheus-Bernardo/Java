12bd3b2af9056eec9c2f6575c669048a551c022d
Matheus Henrique Lourenço Bernardo <matheus.bernardo@inatel.br>
2023-11-09 19:57:02 -0300

    select filmes

--- laboratorio/Locadora/src/main/java/br/inatel/trabalho/Locadora/DAO/FilmeDAO.java
+++ laboratorio/Locadora/src/main/java/br/inatel/trabalho/Locadora/DAO/FilmeDAO.java
@@ -3,6 +3,7 @@ package br.inatel.trabalho.Locadora.DAO;
 import br.inatel.trabalho.Locadora.Models.Filme;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class FilmeDAO extends ConnectionDAO{
     boolean sucesso = false; // sucesso para salvar no banco
@@ -41,5 +42,38 @@ public class FilmeDAO extends ConnectionDAO{
         }
 
         return sucesso;
+    }
+
+    //listar filmes (Select)
+    public ArrayList<Filme> ListarFilmes(){
+        ArrayList<Filme> filmes = new ArrayList<>();
+        conectaNoBanco();
+        String sql = "SELECT idFilme, nome,lancamento  FROM Locadora.Filme;";
+
+        try {
+            st = conexao.prepareStatement(sql);
+            rs = st.executeQuery(sql);
+            System.out.println("lista de filmes");
+            while (rs.next()){
+                Filme filmeaux = new Filme(rs.getInt("idFilme"), rs.getString("nome"),rs.getString("lancamento") );
+                filmes.add(filmeaux);
+                System.out.println("Id do filme: "+filmeaux.getIdFillme());
+                System.out.println("Nome do filme: "+filmeaux.getNomeFilme());
+            }
+            sucesso = true;
+
+
+        }catch (SQLException e){
+            System.out.println("Erro: "+e.getMessage());
+            sucesso= false;
+        }finally {
+            try {
+                conexao.close();
+                st.close();
+            }catch (SQLException e){
+                System.out.println("Erro: "+e.getMessage());
+            }
+        };
+        return filmes;
     }
 }
