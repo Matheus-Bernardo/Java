a1b1893abedee860ca33a28fa3b5b557d88356ab
Matheus Henrique Lourenço Bernardo <matheus.bernardo@inatel.br>
2023-11-10 20:17:30 -0300

    verifica filme-dvd

--- laboratorio/Locadora/src/main/java/br/inatel/trabalho/Locadora/DAO/DvdDAO.java
+++ laboratorio/Locadora/src/main/java/br/inatel/trabalho/Locadora/DAO/DvdDAO.java
@@ -4,6 +4,7 @@ import br.inatel.trabalho.Locadora.Models.Dvd;
 import br.inatel.trabalho.Locadora.Models.Filme;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class DvdDAO extends ConnectionDAO {
 
@@ -13,7 +14,7 @@ public class DvdDAO extends ConnectionDAO {
         //abrir conexão com o banco
         conectaNoBanco();
 
-        String sql = "INSERT INTO dvd(idDvd,Filme_idFilme) values (?,?)";
+        String sql = "INSERT INTO dvd(idDvd,Filme_idFilme,alugado) values (?,?,NULL)";
 
         //parametros a serem inseridos no Banco de dados
 
@@ -43,5 +44,40 @@ public class DvdDAO extends ConnectionDAO {
 
         return sucesso;
     }
+
+    //listar dvds (Select)
+    public ArrayList<Dvd> ListarFilmes() {
+        ArrayList<Dvd> dvds = new ArrayList<>();
+        conectaNoBanco();
+        String sql = "SELECT Dvd.idDvd, Filme.idFilme, Filme.nome " +
+                "FROM Locadora.Dvd " +
+                "JOIN Locadora.Filme ON Dvd.Filme_idFilme = Filme.idFilme " +
+                "WHERE Dvd.alugado IS NULL;";
+
+        try {
+            st = conexao.prepareStatement(sql);
+            rs = st.executeQuery(sql);
+            System.out.println("Lista de DVDs disponíveis para alugar:");
+            while (rs.next()) {
+                Dvd dvdAux = new Dvd(rs.getInt("idDvd"), rs.getInt("idFilme"), null);  // Ajuste conforme sua classe Dvd
+                dvds.add(dvdAux);
+                System.out.println("Id do DVD: " + dvdAux.getIdDvd());
+                System.out.println("Nome do filme gravado no DVD: " + rs.getString("nome"));
+            }
+            sucesso = true;
+        } catch (SQLException e) {
+            System.out.println("Erro: " + e.getMessage());
+            sucesso = false;
+        } finally {
+            try {
+                conexao.close();
+                st.close();
+            } catch (SQLException e) {
+                System.out.println("Erro: " + e.getMessage());
+            }
+        }
+        return dvds;
+    }
+
 
 }
