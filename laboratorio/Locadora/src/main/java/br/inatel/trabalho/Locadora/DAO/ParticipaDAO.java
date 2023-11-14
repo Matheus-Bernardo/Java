8aa819b76664b2b6579990e367869271fdf19022
Matheus Henrique Lourenço Bernardo <matheus.bernardo@inatel.br>
2023-11-14 08:46:27 -0300

    participa ator x filme

---
+++ laboratorio/Locadora/src/main/java/br/inatel/trabalho/Locadora/DAO/ParticipaDAO.java
@@ -0,0 +1,47 @@
+package br.inatel.trabalho.Locadora.DAO;
+
+import br.inatel.trabalho.Locadora.Models.Ator;
+import br.inatel.trabalho.Locadora.Models.Filme;
+
+import java.sql.SQLException;
+
+public class ParticipaDAO extends ConnectionDAO {
+
+    FilmeDAO filmeDAO = new FilmeDAO();
+    AtoresDAO atoresDAO = new AtoresDAO();
+    boolean sucesso = false; // sucesso para salvar no banco
+
+    //função para adicionar atores a um filme
+    public boolean adicionarAtorFilme(int idAtor, int idFilme){
+
+        conectaNoBanco();
+        String sql = "INSERT INTO Participa (Ator_idAtor, Filme_idFilme) VALUES (?, ?)";
+        try{
+            pst = conexao.prepareStatement(sql);
+            pst.setInt(1, idAtor);
+            pst.setInt(2, idFilme);
+
+            pst.execute();
+            sucesso = true;
+            System.out.println("Ator adicionado ao elenco.");
+
+        }catch (SQLException exc){
+            System.out.println("Erro: " + exc);
+
+        }finally {
+            try {
+                conexao.close();//termina a conexão com o banco de dados
+                pst.close();
+                System.out.println("Conexão com o banco fechada");
+            }catch (SQLException exc){
+                System.out.println("Erro: "+ exc);
+            }
+        }
+
+        return sucesso;
+
+
+
+    }
+
+}
